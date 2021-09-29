86983c3e7afb87c4136de5b9047b72283724e70f
Bruno Cesar <brunocesar12386@gmail.com>
2021-09-29 20:52:40 -0300

    create page users

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,123 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Thead,
+  Th,
+  Checkbox, Tbody, Tr, Td, Text
+} from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1400} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar Novo
+            </Button>
+          </Flex>
+          <Table colorScheme="whiteAlpha" >
+            <Thead>
+              <Th px="6" color="gray.300" width="8">
+                <Checkbox colorScheme="pink" />
+              </Th>
+              <Th>Usuários</Th>
+              <Th>Data de cadastro</Th>
+              <Th width="8"></Th>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Bruno Cesar</Text>
+                    <Text fontSize="sm" color="gray.300">brunocesar12386@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>29 de Setembro, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"        
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Bruno Cesar</Text>
+                    <Text fontSize="sm" color="gray.300">brunocesar12386@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>29 de Setembro, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"        
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Bruno Cesar</Text>
+                    <Text fontSize="sm" color="gray.300">brunocesar12386@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>29 de Setembro, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"        
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
